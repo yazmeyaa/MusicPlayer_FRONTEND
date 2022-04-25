4d9170c6301b053307a977de804d8215bcb5dcaf
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-04-25 17:27:56 +0300

    Basic styles?

--- music/src/BottomPlayer/ProgressBar/ProgressBar.tsx
+++ music/src/BottomPlayer/ProgressBar/ProgressBar.tsx
@@ -1,7 +1,15 @@
+import React, { HTMLInputTypeAttribute, useState } from 'react'
+import { ProgressBody, PregressBar } from './styled'
+
 export const ProgressBar = () => {
+    //const [currentProgress, setCurrentProgress] = useState<number>(0)
+    function handleChangeProgress(event:React.ChangeEvent<HTMLInputElement>){
+        console.log(event.target.value)
+    }
+
     return(
-        <div>
-            ProgressBar?
-        </div>
+        <ProgressBody>
+            <PregressBar type={'range'} min='0' max='100' onChange={handleChangeProgress}/>
+        </ProgressBody>
     )
 }
--- music/src/BottomPlayer/ProgressBar/styled.ts
+++ music/src/BottomPlayer/ProgressBar/styled.ts
@@ -2,6 +2,31 @@ import styled from "styled-components";
 
 export const ProgressBody = styled.div`
     &{
+        display: flex;
+        justify-content: center;
         width: 100%;
+        padding-top: .3rem;
+        padding-bottom: .3rem;
+    }
+`
+
+export const PregressBar = styled.input`
+    &{
+        -webkit-appearance: none;
+        width: 80%;
+        box-sizing: border-box;
+    }
+    &::-webkit-slider-runnable-track{
+        background: yellow;
+        border: 1px solid black;
+        border-radius: 8px;
+    }
+    &::-webkit-slider-thumb{
+        -webkit-appearance: none;
+        height: 1rem;
+        width: 1rem;
+        border-radius: 50%;
+        border: 1px solid black;
+        background: black;
     }
 `
