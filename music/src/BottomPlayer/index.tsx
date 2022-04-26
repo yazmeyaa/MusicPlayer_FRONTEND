b356416a72f9f8f35f388ea921b97c3bbbb9b875
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-04-26 20:34:18 +0300

    Transfer branch

--- music/src/BottomPlayer/index.tsx
+++ music/src/BottomPlayer/index.tsx
@@ -2,8 +2,21 @@ import {Container, SubContainer} from './styled'
 import {ButtonsCollection} from './ButtonsCollection/ButtonsCollection'
 import { MusicVolume } from './MusicVolume/MusicVolume'
 import { ProgressBar } from './ProgressBar/ProgressBar'
+import Monster from '../SimpleSongs/monster.mp3'
+import { useEffect, useState } from 'react'
+
 
 export const Player = () => {
+    const [currentSong, setCurrentSong] = useState(new Audio(Monster))
+    const [currentSongTiming, setCurrentSongTiming] = useState(0)
+    const [songDuration, setSongDuration] = useState(0)
+    const [isPlaying, setPlaying] = useState(false)
+    
+    useEffect(()=>{
+        setSongDuration(()=>{
+            return currentSong.duration
+        })
+    },[currentSong])
 
 
     
@@ -13,7 +26,7 @@ export const Player = () => {
                 <ButtonsCollection />
                 <MusicVolume />
             </SubContainer>
-            <ProgressBar />
+            <ProgressBar currentSongProgress={currentSongTiming} songDuration={songDuration} />
         </Container>
     )
 }
