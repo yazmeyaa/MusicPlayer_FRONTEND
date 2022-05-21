609e2ba7591e5502cc56fe96ba2dde3c29119bee
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-05-21 20:12:34 +0300

    basic structure for auth page

--- music/src/Pages/AuthPage/index.tsx
+++ music/src/Pages/AuthPage/index.tsx
@@ -1,67 +1,59 @@
-import { useForm, FieldValues, FieldErrors } from 'react-hook-form'
+import { useForm, FieldErrors, SubmitHandler, FieldValues, SubmitErrorHandler } from 'react-hook-form'
 import axios from 'axios'
 
+interface FormValues {
+    username: string,
+    password: string
+}
+
 export const AuthPage = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
+    const { register, handleSubmit, formState: { errors, isValid }, reset } = useForm<FormValues>({
+        mode: 'onBlur'
+    })
 
-    async function handleRegister(data: FieldValues) {
-
-        class RecievedValue {
-            constructor(private message: string) {
-                this.message = message
-            }
-        }
-
-        const response = await axios.post('http://localhost:3000/api/auth', {
-            username: data.username,
-            password: data.password
-        }
-        ).then(response => {
-            if (response.status === 200 && response.data instanceof RecievedValue) {
-                return response.data
-            } else {
-                throw response.data.error
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
-
-
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        alert(data)
+        reset()
     }
 
-    function handleCatchErrors(error: FieldErrors) {
-
+    const onError: SubmitErrorHandler<FieldErrors> = (error) => {
+        alert(error)
     }
 
     return (
         <div>
             <div>
-                <form onSubmit={handleSubmit(handleRegister, handleCatchErrors)} >
-                    <input {...register('username', {
-                        required: 'Username is required',
-                        minLength: {
-                            value: 3,
-                            message: 'Username cant be less then 3 symbols'
-                        },
-                        maxLength: {
-                            value: 36,
-                            message: 'Username cannot be described 36 characters'
-                        }
-                    })} />
-                    <input {...register('password',
-                        {
-                            required: 'Password is required',
+                <form onSubmit={handleSubmit(onSubmit, onError)} >
+                    <label >
+                        Username
+                        <input {...register('username', {
+                            required: 'Username is required',
                             minLength: {
-                                value: 8,
-                                message: 'Password cant be less then 3 symbols'
+                                value: 3,
+                                message: 'Username cant be less then 3 symbols'
                             },
                             maxLength: {
-                                value: 64,
-                                message: 'Password cannot be described 36 characters'
+                                value: 36,
+                                message: 'Username cannot be described 36 characters'
                             }
                         })} />
-                    <button >register</button>
+                    </label>
+                    <label>
+                        Password
+                        <input {...register('password',
+                            {
+                                required: 'Password is required',
+                                minLength: {
+                                    value: 8,
+                                    message: 'Password cant be less then 3 symbols'
+                                },
+                                maxLength: {
+                                    value: 64,
+                                    message: 'Password cannot be described 36 characters'
+                                }
+                            })} />
+                    </label>
+                    <button type='submit' disabled={!isValid} >register</button>
                 </form>
             </div>
         </div>
