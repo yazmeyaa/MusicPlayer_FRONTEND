a925820c01678b5aa41b15b51064fd32847e432a
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-03 13:27:55 +0300

    App config

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,10 +1,15 @@
-const { REACT_APP_BASE_URL: baseUrl, REACT_APP_PORT: port } = process.env;
+const {
+  REACT_APP_BASE_URL: baseUrl,
+  REACT_APP_PORT: port,
+  REACT_APP_BACKEND_URL: backendUrl,
+} = process.env;
 
 const getValueByName = (value: string | undefined, defaultValue = '') => {
   return value || defaultValue;
 };
 
 export const appConfig = {
-  baseUrl: getValueByName(baseUrl, 'http://localhost: 8080'),
+  baseUrl: getValueByName(baseUrl, 'http://localhost:8080'),
   port: getValueByName(port),
+  backendUrl: getValueByName(backendUrl),
 };
