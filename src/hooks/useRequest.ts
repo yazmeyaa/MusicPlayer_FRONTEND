1bcfd71e2aac4250b7ed6a8367da5bad35886806
yazmeyaa <vip.tonuvmartini@mail.ru>
2022-06-03 14:20:04 +0300

    Added useRequest hook

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,3 +1,27 @@
+import axios, { Method } from 'axios';
+import { useState, useCallback } from 'react';
+
+interface IRequestArgs {
+  URL: string;
+  method: Method;
+  data?: Object;
+}
+
 export const useRequest = () => {
-  return '123';
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const request = useCallback(async ({ URL, method, data }: IRequestArgs) => {
+    setLoading(true);
+    const response = await axios({
+      url: URL,
+      method: method,
+      data: data,
+    }).then((data) => {
+      return data;
+    });
+    setLoading(false);
+    return response;
+  }, []);
+
+  return [loading, request];
 };
